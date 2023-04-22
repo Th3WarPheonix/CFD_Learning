ff3e044d37ed98e4428d9a676e47ed4c04ff6115
Th3WarPheonix <113734956+Th3WarPheonix@users.noreply.github.com>
2023-04-22 17:22:16 -0400

    finish backward started forward

--- Book Excercises/Excerices_5-1.py
+++ Book Excercises/Excerices_5-1.py
@@ -7,41 +7,52 @@ un1 is the value of the node at the next time step
 un  is the value of the node at the current time step
 CFL = cdt/dx"""
 
-def explicit_backward(value_array, CFL_num, time_steps):
+def explicit_backward(domain_array, value_array, advection_coeff, deltat, time_steps):
     uns = np.copy(value_array)
     un1s = np.copy(value_array)
+
     for t in range(time_steps):
-        for i, uni in enumerate(uns):
-            un1s[i] = uni - CFL_num
+        for i in range(1, len(domain_array)):
+            un1s[i] = uns[i] - advection_coeff*deltat*(uns[i]-uns[i-1])/(domain_array[i]-domain_array[i-1])
 
-def explicit_forward(CFL_num):
+        uns = np.copy(un1s)
+    return un1s
+
+def explicit_forward(domain_array, value_array, advection_coeff, deltat, time_steps):
+    uns = np.copy(value_array)
+    un1s = np.copy(value_array)
+
+    for t in range(time_steps):
+        for i in range(1, len(domain_array)):
+            un1s[i] = uns[i] - advection_coeff*deltat*(uns[i+1]-uns[i])/(domain_array[i]-domain_array[i-1])
+        uns = np.copy(un1s)
+    return un1s
+
+def explicit_central():
     pass
 
-def explicit_central(CFL_num):
+def implicit_central():
     pass
 
-def implicit_central(CFL_num):
+def crank_nicolson():
     pass
 
-def crank_nicolson(CFL_num):
+def lax_method():
     pass
 
-def lax_method(CFL_num):
+def lax_wendroff():
     pass
 
-def lax_wendroff(CFL_num):
+def maccormack_method():
     pass
 
-def maccormack_method(CFL_num):
+def jameson_method():
     pass
 
-def jameson_method(CFL_num):
+def warming_beam():
     pass
 
-def warming_beam(CFL_num):
-    pass
-
-def upwind_method(CFL_num):
+def upwind_method():
     pass
 
 def initial_condition(value1, value2, x_sep, x_start, x_end, num_nodes):
@@ -53,14 +64,27 @@ def initial_condition(value1, value2, x_sep, x_start, x_end, num_nodes):
 
 
 def main():
-   domain, values = initial_condition(1, .5, .5, 0, 2, 41)
+    CFL_num = 0.9
+    advection_coeff = 1
+    num_points = 40
+    x_start = 0
+    x_end = 2
+    deltat = CFL_num*(x_end-x_start)/num_points*advection_coeff # The expression (domain[0] - domain[1])/advection_coeff should really be a minimum over the spacing of all points
+    
+    domain, values_ic = initial_condition(1, .5, .5, 0, 2, num_points+1) # set up inital conditions
 
-   plt.plot(domain, values, label='Initial Condition', linestyle='--')
-   plt.xlabel('x')
-   plt.ylabel('u')
-   plt.legend()
-   plt.show()
-   
+    values_eb = explicit_backward(domain, values_ic, advection_coeff, deltat, 10)
+    values_ef = explicit_forward(domain, values_ic, advection_coeff, deltat, 10)
+
+
+    plt.plot(domain, values_eb, label='Explicit Backward', marker='.')
+    plt.plot(domain, values_ef, label='Explicit Forward', marker='.')
+    plt.plot(domain, values_ic, label='Initial Condition', linestyle='--')
+    plt.xlabel('x')
+    plt.ylabel('u')
+    plt.legend()
+    plt.show()
+    
 
 if __name__ == "__main__":
     main()
